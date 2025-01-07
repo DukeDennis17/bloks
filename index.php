e3f2f84e0f8bbee6c9c0dc5316af43baccc1b62e
DukeDennis17 <ipb23.a.valters@vtdt.edu.lv>
2025-01-07 14:50:11 +0200

    Inital commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+include "helper.php";
+include "database.php";
+
+$config = require ("config.php");
+
+$db = new database($config["Database"]);
+
+$sql = "SELECT * FROM posts";
+$params = [];
+if (isset($_GET["search_query"]) &&$_GET["search_query"] != ""){
+    // echo"fent reactor";
+    $search_query = "%" . $_GET["search_query"] . "%";
+    $sql .= " WHERE content LIKE :search_query;"; // sagatavotais vaicajums
+    $params = ["search_query" => $search_query]; //saistitais parametrs
+} 
+
+$posts = $db->query($sql, $params)->fetchall();
+    
+echo "<form>";
+echo "<input name='search_query' />";
+echo "<button>meklet</button>";
+echo "</form>";
+
+echo"<ul>";
+if(count($posts) == 0 ){
+    echo "nav atrasts pamegini citu";
+}
+echo"<ul>";
+foreach($posts as $post){
+    echo "<li>" . $post["content"] . "</li>";
+};
+echo "</ul>";
