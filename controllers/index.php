5e8ea05b9100a394b51362c6e76d123b328cff19
Skolnieks <skolnieks.pjvt@pjvt.local>
2025-01-14 13:30:19 +0200

    hah

---
+++ controllers/index.php
@@ -0,0 +1,14 @@
+<?php
+
+$sql = "SELECT * FROM posts";
+$params = [];
+if (isset($_GET["search_query"]) &&$_GET["search_query"] != ""){
+    // echo"fent reactor";
+    $search_query = "%" . $_GET["search_query"] . "%";
+    $sql .= " WHERE content LIKE :search_query;"; // sagatavotais vaicajums
+    $params = ["search_query" => $search_query]; //saistitais parametrs
+} 
+
+$posts = $db->query($sql, $params)->fetchall();
+    $pageTitle = "Blogs";
+require "views/index.view.php";
--- index.php
+++ index.php
@@ -6,15 +6,4 @@ $config = require ("config.php");
 
 $db = new database($config["Database"]);
 
-$sql = "SELECT * FROM posts";
-$params = [];
-if (isset($_GET["search_query"]) &&$_GET["search_query"] != ""){
-    // echo"fent reactor";
-    $search_query = "%" . $_GET["search_query"] . "%";
-    $sql .= " WHERE content LIKE :search_query;"; // sagatavotais vaicajums
-    $params = ["search_query" => $search_query]; //saistitais parametrs
-} 
-
-$posts = $db->query($sql, $params)->fetchall();
-    $pageTitle = "Blogs";
-require "views/index.view.php";
+require "router.php";
